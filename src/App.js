c85fe33ee440dd0191bd7972c6cf0dd917ddda95
mimizae <jim400934@gmail.com>
2025-10-31 20:15:40 +0900

    feat: 사용자 입력 값 핸들링 로직 흐름 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    try {
+      const purchaseAmount = await MissionUtils.Console.readLineAsync(
+        "구입금액을 입력해 주세요.\n"
+      );
+      // 구입금액 검증
+      const validatedAmount = this.#validatePurchaseAmount(purchaseAmount);
+
+      const lottoCount = validatedAmount / 1000;
+      MissionUtils.Console.print(`\n${lottoCount}개를 구매했습니다.\n`);
+      // 로또 출력 함수 호출
+
+      const winningNumbers = await MissionUtils.Console.readLineAsync(
+        "\n당첨 번호를 입력해 주세요.\n"
+      );
+
+      // 당첨 번호 검증
+
+      const bonusNumber = await MissionUtils.Console.readLineAsync(
+        "\n보너스 번호를 입력해 주세요.\n"
+      );
+
+      // 보너스 번호 검증
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error;
+    }
+  }
+  #validatePurchaseAmount(amount) {}
 }
 
 export default App;
