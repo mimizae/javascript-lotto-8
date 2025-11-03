3b55f178132cda8dfc4cce717bd9600aec116cdf
mimizae <jim400934@gmail.com>
2025-11-03 14:28:48 +0900

    feat: App.js에  로또 결과 계산 후 출력하는 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import InputView from "./Views/InputView";
 import OutputView from "./Views/OutputView";
+import PurchaseAmount from "./Validators/PurchaseAmount";
+import LottoMachine from "./Features/LottoMachine";
 import WinningNumbers from "./Validators/WinningNumbers";
 import BonusNumber from "./Validators/BonusNumber";
+import LottoResult from "./Features/LottoResult";
 
 class App {
   async run() {
     try {
       const purchaseAmountInput = await InputView.readPurchaseAmount();
-      // 구입 금액 검증
-      const validatedAmount = this.#validatePurchaseAmount(purchaseAmountInput);
+      // TODO: 검증 로직을 담은 클래스들의 파일명 적절한지? 점검
+      const validatedAmount = PurchaseAmount.validate(purchaseAmountInput);
 
       const lottoCount = validatedAmount / 1000;
       OutputView.printLottoCount(lottoCount);
 
       // OutputView.js에서 로또 출력 함수 호출
+      const lottos = LottoMachine.generateLottos(validatedAmount);
+      OutputView.printLottoNumbers(lottos);
 
       const winningNumbersInput = await InputView.readWinningNumbers();
 
@@ -25,16 +30,23 @@ class App {
       const bonusNumberInput = await InputView.readBonusNumber();
 
       // 보너스 번호 검증
-      const validatedbonusNumber = BonusNumber.validate(
+      const validatedBonusNumber = BonusNumber.validate(
         bonusNumberInput,
         validatedWinningNumbers
       );
+
+      const result = new LottoResult(
+        lottos,
+        validatedWinningNumbers,
+        validatedBonusNumber
+      );
+
+      OutputView.printResult(result.getResultMap());
+      OutputView.printEarningRate(result.getEarningRate(validatedAmount));
     } catch (error) {
       MissionUtils.Console.print(error.message);
-      throw error;
     }
   }
-  #validatePurchaseAmount(amount) {}
 }
 
 export default App;
